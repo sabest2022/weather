53a6b4a9429bc4c19894af55af645e4436e6f6b6
Yohanna Sium <yohannasium@Yohannas-MacBook-Air.local>
2023-12-06 19:33:45 +0100

    Adding react router for navigation and corresponding pages

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,12 +1,31 @@
 import Main from './pages/Main/Main'
 import Profile from './pages/Profile/Profile'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+import { createBrowserRouter, RouterProvider} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />
+  },
+  {
+    path: "/profile",
+    element: <Profile />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  }
+])
 
 function App() {
   return (
-    <>
-      <Main />
-      <Profile />
-    </>
+    <RouterProvider router={router} />
   )
 }
 
