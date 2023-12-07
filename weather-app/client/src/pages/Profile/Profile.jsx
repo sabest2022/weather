f71d57ebf8c2dbc6643c0731d9576a7aca592eb1
Yohanna Sium <yohannasium@Yohannas-MacBook-Air.local>
2023-12-07 21:05:04 +0100

    Adding profile page layout for sprint 1.

--- weather-app/client/src/pages/Profile/Profile.jsx
+++ weather-app/client/src/pages/Profile/Profile.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 import './Profile.scss'
 
 const Profile = () => {
-  return <h1>Profile page</h1>
+  return (
+    <div className='profile'>
+      <h3>Profile page</h3>
+      <img src="https://robohash.org/yohanna" alt="Profile image" />
+      <ul>
+       <li>ID: "ID GOES HERE"</li>
+       <li>Username: "John Doe"</li>
+       <li>Email: "john@gmail.com"</li>
+       <li>Subscription: "IS USER SUBSCRIBED?"</li>
+      </ul>
+      <button>Sign out</button>
+    </div>
+  )
 }
 
 export default Profile
