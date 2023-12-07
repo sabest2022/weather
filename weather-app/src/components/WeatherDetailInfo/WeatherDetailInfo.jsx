68669fe604b44c82266c78d3fddc9e482348b2c6
Naviddev1 <Navid86@hotmail.se>
2023-12-07 12:51:33 +0100

    some basic structires done

--- weather-app/src/components/WeatherDetailInfo/WeatherDetailInfo.jsx
+++ weather-app/src/components/WeatherDetailInfo/WeatherDetailInfo.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import './WeatherDetailInfo.scss'
 
+import frost_pic from '../../assets/Frost_weather.png'
+import snow_pic from '../../assets/Snow_weather.png'
+import sunny_pic from '../../assets/Sunny_weather.png'
+import search_icon from '../../assets/Search_icon.png'
+
 const WeatherDetailInfo = () => {
-  return <div>WeatherDetailInfo</div>
+  return (
+    <div className="container">
+      WeatherDetailInfo
+      <div className="top-bar">
+        <input type="text" className="city-input" placeholder="Search City" />
+        <div className="search-icon">
+          <img src={search_icon} alt="" />
+        </div>
+      </div>
+      <div className="weather-image">
+        <img src={sunny_pic} alt="" />
+      </div>
+    </div>
+  )
 }
 
 export default WeatherDetailInfo
