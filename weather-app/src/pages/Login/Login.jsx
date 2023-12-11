34ecc6e028fe914b02e61f24da0ae2396f4dd198
sabest2022 <askarians@me.com>
2023-12-11 14:05:31 +0100

    google login init

--- weather-app/src/pages/Login/Login.jsx
+++ weather-app/src/pages/Login/Login.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
-const Login = () => {
+import { GoogleLogin } from 'react-google-login'
+const clientId = '152826738328-2gschac9945q44ilfue2n9c6d19nt296.apps.googleusercontent.com';
+function Login() {
+  const onSuccess = (res) => {
+    console.log('Login Success! Current user: ', res.profileObj);
+  }
+  const onFailure = (res) => {
+    console.log('LoginFailed! res: ', res);
+  }
   return (
-    <div>Login</div>
+    <div id="signinbutton">
+
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Google Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        isSignedIn={true}
+      />
+    </div>
   )
 }
-
-export default Login
+export default Login;
--- weather-app/src/pages/Main/Main.jsx
+++ weather-app/src/pages/Main/Main.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import './Main.scss'
+import { useEffect } from 'react'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import WeatherList from '../../components/WeatherList/WeatherList'
 import WeatherDetailInfo from '../../components/WeatherDetailInfo/WeatherDetailInfo'
-
+import LoginButton from '../Login/Login';
+import { gapi } from 'gapi-script'
+const clientId = "152826738328-2gschac9945q44ilfue2n9c6d19nt296.apps.googleusercontent.com";
 const Main = () => {
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        scope: ""
+      })
+    };
+    gapi.load('client:auth2', start);
+  });
+
   return (
     <main>
       <Sidebar />
       <div className="main-wrapper">
         <div className="main-buttons">
           <button>Theme</button>
-          <button>Login</button>
+          <LoginButton />
           <button>Register</button>
         </div>
         <div className="main-content">
