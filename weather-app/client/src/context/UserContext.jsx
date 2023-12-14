23b40d6e12bf9fa09c286d1037d54ad23fc2e065
sabest2022 <askarians@me.com>
2023-12-14 17:51:33 +0100

    moved all logic and req. to UserContext

--- weather-app/client/src/context/UserContext.jsx
+++ weather-app/client/src/context/UserContext.jsx
@@ -1,13 +1,62 @@
-import React, { createContext, useContext, useState } from 'react';
+
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [isSignedIn, setIsSignedIn] = useState(false);
     const [currentUser, setCurrentUser] = useState(null);
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
+    const checkAuthStatus = async () => {
+        try {
+            const { data } = await axios.get('http://localhost:3000/api/google-authorize', { withCredentials: true });
+            setCurrentUser(data);
+            setIsSignedIn(true);
+        } catch (error) {
+            setCurrentUser(null);
+            setIsSignedIn(false);
+        }
+    };
+
+    useEffect(() => {
+
+        checkAuthStatus();
+    }, []);
+
+    const login = async (tokenId) => {
+        try {
+            const response = await axios.post(
+                'http://localhost:3000/api/google-login',
+                { token: tokenId },
+                { withCredentials: true }
+            );
+            if (response.data) {
+                checkAuthStatus(); // Refresh user data
+            }
+        } catch (error) {
+            console.error('Server error during login:', error);
+        }
+    };
+
+    const logout = async () => {
+        try {
+            const response = await axios.post(
+                'http://localhost:3000/api/google-logout',
+                null,
+                { withCredentials: true }
+            );
+            if (response.status === 204) {
+                setCurrentUser(null);
+                setIsSignedIn(false);
+            }
+        } catch (error) {
+            console.error('Server error during logout:', error);
+        }
+    };
 
     return (
-        <UserContext.Provider value={{ isSignedIn, setIsSignedIn, currentUser, setCurrentUser }}>
+        <UserContext.Provider value={{ currentUser, setCurrentUser, isSignedIn, setIsSignedIn, login, logout }}>
             {children}
         </UserContext.Provider>
     );
--- weather-app/client/src/pages/Login/Login.jsx
+++ weather-app/client/src/pages/Login/Login.jsx
@@ -1,76 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import { GoogleLogin, GoogleLogout } from 'react-google-login'
-import axios from 'axios'
+import React from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { useUserContext } from '../../context/UserContext';
 
-const clientId =
-  '152826738328-2gschac9945q44ilfue2n9c6d19nt296.apps.googleusercontent.com'
+const clientId = '152826738328-2gschac9945q44ilfue2n9c6d19nt296.apps.googleusercontent.com';
 
 function Login() {
-  const { isSignedIn, setIsSignedIn, currentUser, setCurrentUser } = useUserContext();
-
-  // const [user, setCurrentUser] = useState(null)
-  // const [isSignedIn, setIsSignedIn] = useState(false)
-  // console.log(user)
-  const checkAuthStatus = async () => {
-    try {
-      const { data } = await axios.get(
-        'http://localhost:3000/api/google-authorize',
-        { withCredentials: true },
-      )
-      setCurrentUser(data)
-      setIsSignedIn(true)
-    } catch (error) {
-      setCurrentUser(null)
-      setIsSignedIn(false)
-    }
-  }
-
-  useEffect(() => {
-    checkAuthStatus()
-  }, [])
+  const { isSignedIn, login, logout } = useUserContext();
 
   const onSuccess = async (res) => {
-    await axios
-      .post(
-        'http://localhost:3000/api/google-login',
-        { token: res.tokenId },
-        { withCredentials: true },
-      )
-      .then((response) => {
-        console.log('Server response:', response.data)
-        checkAuthStatus()
-      })
-      .catch((error) => {
-        console.error('Server error', error)
-      })
-  }
+    await login(res.tokenId);
+  };
 
   const onFailure = (res) => {
-    console.log('Login Failed! res: ', res)
-  }
+    console.log('Login Failed! res: ', res);
+  };
 
   const onLogoutSuccess = async () => {
-    try {
-      const response = await axios.post(
-        'http://localhost:3000/api/google-logout',
-        null,
-        {
-          withCredentials: true,
-        },
-      )
-
-      if (response.status === 204) {
-        setCurrentUser(null)
-        setIsSignedIn(false)
-        console.log('Logout was successful')
-      } else {
-        console.error('Logout failed with status:', response.status)
-      }
-    } catch (error) {
-      console.error('Server error during logout', error)
-    }
-  }
+    await logout();
+  };
 
   return (
     <div id="signinbutton">
@@ -90,7 +37,7 @@ function Login() {
         />
       )}
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
