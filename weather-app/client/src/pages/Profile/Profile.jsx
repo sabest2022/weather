06cf33282879c35fbe1c6aab3043a7bc81613cf9
RickardTimergalejevs <rickard.time@gmail.com>
2023-12-14 18:16:43 +0100

    style: profile page and plans for balance

--- weather-app/client/src/pages/Profile/Profile.jsx
+++ weather-app/client/src/pages/Profile/Profile.jsx
@@ -1,28 +1,37 @@
-import React, { useState, useEffect} from 'react'
-import { Link } from "react-router-dom"
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import './Profile.scss'
 
 const Profile = () => {
   const [user, setUser] = useState({})
 
   useEffect(() => {
-    const currentUser = JSON.parse(localStorage.getItem("currentUser"))
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'))
     setUser(currentUser)
   }, [])
 
   return (
-    <div className='profile'>
-      <h3>Profile page</h3>
-      <img src={user.imageUrl} alt="Profile image" />
-      <ul>
-       <li>ID: {user.googleId}</li>
-       <li>Username: {user.name}</li>
-       <li>Email: {user.email}</li>
-      </ul>
-     <Link to="/">
-     <button>Home</button>
-     </Link>
-    </div>
+    <main>
+      <div className="profile-wrapper">
+        <div className="user-info-wrapper">
+          <img src={user.imageUrl} alt="Profile image" />
+          <ul>
+            <li>ID: {user.googleId}</li>
+            <li>Username: {user.name}</li>
+            <li>Email: {user.email}</li>
+          </ul>
+          <Link to="/">
+            <button>Home</button>
+          </Link>
+        </div>
+        <div className="plans-list">
+          <h1>Top up your balance with</h1>
+          <button>100</button>
+          <button>250</button>
+          <button>500</button>
+        </div>
+      </div>
+    </main>
   )
 }
 
