005e64f5b835fe7d30b91ac0d77910b6f271afee
Naviddev1 <Navid86@hotmail.se>
2023-12-14 20:58:59 +0100

    ✅⚠️ deleted some routes and files the team deemed abundent

--- weather-app/client/src/App.jsx
+++ weather-app/client/src/App.jsx
@@ -1,7 +1,5 @@
 import Main from './pages/Main/Main'
 import Profile from './pages/Profile/Profile'
-import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { WeatherProvider } from './context/WeatherContext'
 
@@ -13,14 +11,6 @@ const router = createBrowserRouter([
   {
     path: '/profile',
     element: <Profile />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/register',
-    element: <Register />,
   },
 ])
 
